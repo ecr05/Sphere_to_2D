correcting normalisation

backprojectresults_autoencoder_dHCP3D.py
@@ -16,14 +16,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 normalised=False
-orig_data=False
-num_test=len(os.listdir(paths.outputdir))
+orig_data=True
 
 # load template data
 surf=nibabel.load(paths.surfname)
 #template=nibabel.load(paths.templategiftiname)
+# load  labels - constant for all subjects - group parcellation
+labels=nibabel.load(paths.labelname)
 
 coordinates=surf.darrays[0].data
+
 
 # get cartesion coordinates for point at zero longitude 90 degrees latitude
 zerocoord=intp.spherical_to_cartesian(100,0,np.pi/2)
@@ -33,7 +35,7 @@ zerocoord=intp.spherical_to_cartesian(100,0,np.pi/2)
 #if normalised:
 #    test_filenames =pd.read_pickle('/data/PROJECTS/dHCP/PROCESSED_DATA/githubv1.1/TESTINGDATA/featuresets/projectedvisL/TESTINGoutname-lookupnormalised.pk1')
 #else:
-test_filenames =pd.read_pickle(os.path.join(paths.bp_paths['Odir'],paths.bp_paths['csv']))
+test_filenames =pd.read_pickle(os.path.join(paths.bp_paths['Odir'],paths.bp_paths['data_csv']))
 # re-project data back from plane to sphere
 # do this for every projection view and merge results (**hack!**)
 ptinds=[]#zerocoord]
@@ -41,7 +43,8 @@ ptinds=[]#zerocoord]
 # then rotate sphere to centre on different label regions
 for i in paths.projection_centres:
     print('projection centre:', i)
-    ptinds.append(coordinates[i])
+    x=np.where(labels.darrays[0].data==i)[0]
+    ptinds.append(coordinates[x[0]])
 
 # create array for each subject to save results of project
 
@@ -61,6 +64,7 @@ for aug in np.arange(len(ptinds)):
         uniquevals = []
         if orig_data==True:
             fname =row['data']
+            print('data',fname )
         else:
             fname = os.path.join(paths.outputdir,paths.outputname + str(row['id'])+'_'+str(row['session']) + '.npy')
        

dHCPmultimodal_paths.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-hemi='left'
+hemi='right'
 if hemi== 'left':
     hemi_template='L'
 else :
@@ -49,26 +49,34 @@ else:
     projection_centres=[15]
     
     
-training_paths = {'Ldir': os.path.normpath(testingdir+ '/labels/'),
+training_paths = {'Ldir': os.path.normpath(trainingdir+ '/labels/'),
                   'Fdir': os.path.normpath(trainingdir+ '/featuresets/'),
-                  'Odir': os.path.normpath(trainingdir+ '/featuresets/projectedcentre-'+ str(projection_centres[0])),
+                  'Odir': os.path.normpath(trainingdir+ '/featuresets/projectedcentre_'+ str(projection_centres[0])),
                   'list': np.genfromtxt(TRAINING , dtype=str),
-                  'csv': 'TRAIN_prem_vs_term.pk1',
+                  'meta_csv': os.path.join(trainingdir, 'TRAIN_prem_vs_term.pk1'),
+                  'data_csv': 'TRAIN_prem_vs_term_data_csv_'+hemi+'.pk1',
                   'abr': 'TRAINING'}
 
 testing_paths = { 'Ldir': os.path.normpath(testingdir+ '/labels/'),
                   'Fdir': os.path.normpath(testingdir+ '/featuresets/'),
-                  'Odir': os.path.normpath(testingdir+ '/featuresets/projectedcentre-'+ str(projection_centres[0])),
+                  'Odir': os.path.normpath(testingdir+ '/featuresets/projectedcentre_'+ str(projection_centres[0])),
                   'list': np.genfromtxt(TESTING , dtype=str),
+                  'meta_csv': os.path.join(testingdir,'TEST_prem_vs_term.pk1'),
+                  'data_csv':'TEST_prem_vs_term_data_csv_'+hemi+'.pk1',
                   'abr': 'TESTING'}
 
 # save projection centres
-np.savetxt(training_paths['Odir'],projection_centres)
-np.savetxt(testing_paths['Odir'],projection_centres)
+
+os.mkdir(training_paths['Odir'])
+os.mkdir(testing_paths['Odir'])
+
+
+np.savetxt(os.path.join(training_paths['Odir'],'projection_centres.txt'),projection_centres)
+np.savetxt(os.path.join(testing_paths['Odir'],'projection_centres.txt'),projection_centres)
 
 
 # for back projection debugging
-bp_paths=training_paths
+bp_paths=testing_paths
 
 
 # tuning parameters
@@ -87,4 +95,4 @@ usegrouplabels = False # use the group average labels for all subjects (projecte
 getFeatureCorrelations = False #useful when using group labels as it can be used to filter training data (see Glasser et al, Nature 2016)
 normalise = False # necessary for old version
 group_normalise=True
-remove_outliers=True
+remove_outliers=False

mapping.py
@@ -16,7 +16,7 @@ import numpy as np
 import copy
 import matplotlib.pyplot as plt 
 import Interpolation as intp
-
+import pandas as pd
 
 def sliceplane(plane, start_height, start_width, x_size, y_size):
     # slice patch from 3D array dim (x,y,channels)
@@ -178,8 +178,9 @@ def invert_patch_categories_full(img,coords,interp,newH,newW,lons,labels=[]):
 def remove_outliers(d,data):
     # quick hack to get rid of xtreme outliers (before there was some really high values)
     if  (np.max(data)>np.mean(data)+10*np.std(data)) or (np.min(data)<np.mean(data)-10*np.std(data)):
+        print(d,  'data stats:', np.mean(data), np.std(data), np.max(data), np.min(data),np.mean(data)+10*np.std(data),np.mean(data)-10*np.std(data))
+
         data[np.where(data>np.mean(data)+10*np.std(data))]= np.mean(data)
-        print(d,  'data stats:', np.mean(data), np.std(data), np.max(data), np.min(data),np.mean(data)+10*np.std(data))
     
 
 def reformat_datamat_rows_to_columns(ALLDATA):
@@ -187,8 +188,8 @@ def reformat_datamat_rows_to_columns(ALLDATA):
     
     for f in range(ALLDATA.features):
         for s in range(ALLDATA.samples):
-            #print(f,s,'ALLDATA.DATA[:,f*s] shape',s*ALLDATA.DATA.shape[0],(s+1)*ALLDATA.DATA.shape[0],newmat.shape)
-            newmat[s*ALLDATA.DATA.shape[0]:(s+1)*ALLDATA.DATA.shape[0],f]= ALLDATA.DATA[:,f*s]       
+            #print(f,s,'ALLDATA.DATA[:,f*s] shape',s*ALLDATA.DATA.shape[0],(s+1)*ALLDATA.DATA.shape[0],newmat.shape,ALLDATA.DATA.shape[0],ALLDATA.DATA[:,f*s].shape,newmat[s*ALLDATA.DATA.shape[0]:(s+1)*ALLDATA.DATA.shape[0],f].shape)
+            newmat[s*ALLDATA.DATA.shape[0]:(s+1)*ALLDATA.DATA.shape[0],f]= ALLDATA.DATA[:,s*ALLDATA.features+f]       
     
     return newmat
 
@@ -198,17 +199,35 @@ def reformat_datamat_columns_to_rows(datamat,ALLDATA):
     for f in range(ALLDATA.features):
         for s in range(ALLDATA.samples):
             #print(f,s,'datamat[s*ALLDATA.DATA.shape[0]:,f]',datamat[s*ALLDATA.DATA.shape[0]:(s+1)*ALLDATA.DATA.shape[0],f].shape,newmat[:,f*s].shape)
-            newmat[:,f*s]= datamat[s*ALLDATA.DATA.shape[0]:(s+1)*ALLDATA.DATA.shape[0]:,f]       
+            newmat[:,s*ALLDATA.features+f]= datamat[s*ALLDATA.DATA.shape[0]:(s+1)*ALLDATA.DATA.shape[0]:,f]       
     
     return newmat
 
 def group_normalise(ALLDATA):
+    """
+          normalise data across group; first restructures data s.t. all examples for each feature fit into one column
+          i.e. (n_subjects*n_vertices,n_features)
+          then it normalises and returns data to (n_vertices,n_subjects*n_features) format
 
+          Parameters
+          ----------
+          ALLDATA: data for all subjects in (n_vertices,n_subjects*n_features) format
+
+          Returns
+          -------
+          
+    """
+    #alldatamean=np.mean(ALLDATA.DATA,axis=0)
     newmat=reformat_datamat_rows_to_columns(ALLDATA)
     newmat=normalize(newmat,0) # normalise along each column
-    newmat=reformat_datamat_columns_to_rows(newmat,ALLDATA)
-    
-    ALLDATA._replace(DATA=newmat)
+    newmat2=reformat_datamat_columns_to_rows(newmat,ALLDATA)
+# =============================================================================
+#     diff=ALLDATA.DATA-newmat2
+#     print('diff',np.sum(diff))
+# =============================================================================
+    #print('group_normalise_mean',np.mean(newmat,axis=0))
+    #ALLDATA._replace(DATA=newmat)
+    return newmat2
 
 def project(DATAset, interp, nlats, nlons, lons):
     """
@@ -280,7 +299,7 @@ def project_data(DATA,interp,outdir,newH,newW,lons,abr,aug,usegroup,use_normalis
         
     start=0    
     for subj in range(0,alldata.samples):
-        print('subj', subj, DATA['data'].ids[subj] )
+        #print('subj', subj, DATA['data'].ids[subj] )
         if 'labels' in DATA and usegroup==False:
             np.save(os.path.join(outdir,abr +'GrayScaleLabels-subj-'+ DATA['data'].ids[subj] + '-aug-' + aug+'-Nature'), twoDL[:,:,subj])
 
@@ -291,14 +310,20 @@ def project_data(DATA,interp,outdir,newH,newW,lons,abr,aug,usegroup,use_normalis
             normalised_data = normalize(twoD[:, :, start:start + alldata.features])
             np.save(os.path.join(outdir, abr + 'data_-subj-' + DATA['data'].ids[subj]  + '-aug-' + aug + 'normalised'),normalised_data)
         else:
+# =============================================================================
+#             data=twoD[:, :, start:start + alldata.features].reshape((320x240,3))
+#             if np.sum(data,axis=0)
+# =============================================================================
+            
             np.save(os.path.join(outdir, abr + 'data_-subj-' + DATA['data'].ids[subj]  + '-aug-' + aug), twoD[:, :, start:start + alldata.features])
+            
         start = start+alldata.features
 
     if 'correlations'  in DATA:
         np.save(os.path.join(outdir,abr +'meanfeaturecorrelations-aug-' + aug), twoDcorr[:,:,alldata.samples])
 
 
-def write_projection_paths(DATA, filename, indir, abr, aug, use_grouplabels, use_correlations,use_normalisation):
+def write_projection_paths(DATA, filename, indir, abr, aug, use_labels, use_grouplabels, use_correlations,use_normalisation,meta_csv):
     """
         write paths out to file
 
@@ -314,14 +339,17 @@ def write_projection_paths(DATA, filename, indir, abr, aug, use_grouplabels, use
          Returns
          -------
        """
-    target = open(filename, 'w')
+    meta_data=pd.read_pickle(meta_csv)
     
+    df = pd.DataFrame() 
+    print('write projection paths')
     for subj in range(0,DATA.samples):
-        print('subj', subj, DATA.ids[subj],type(DATA.ids[subj]) )        
-        if use_grouplabels == True:
-            label = os.path.join(indir, abr + 'GrayScaleLabels-group-aug-' + aug +'.npy')
-        else:
-            label = os.path.join(indir, abr + 'GrayScaleLabels-subj-' + DATA.ids[subj]+ '-aug-' + aug + '-Nature.npy')
+        #print('subj', subj, DATA.ids[subj],type(DATA.ids[subj]) )        
+        if use_labels:
+            if use_grouplabels == True:
+                label = os.path.join(indir, abr + 'GrayScaleLabels-group-aug-' + aug +'.npy')
+            else:
+                label = os.path.join(indir, abr + 'GrayScaleLabels-subj-' + DATA.ids[subj]+ '-aug-' + aug + '-Nature.npy')
 
         if use_normalisation:
             print(os.path.join(indir, abr + 'data_-subj-' + DATA.ids[subj] + '-aug-' + aug+ 'normalised.npy'))
@@ -329,10 +357,25 @@ def write_projection_paths(DATA, filename, indir, abr, aug, use_grouplabels, use
         else:
             data = os.path.join(indir, abr + 'data_-subj-' +DATA.ids[subj]+ '-aug-' + aug + '.npy')
 
-        if use_correlations:
-            corrdata=os.path.join(indir, abr + 'featurecorrelations-subj-' +DATA.ids[subj] + '-aug-' + aug + '.npy')
-            meancorrdata=os.path.join(indir, abr + 'meanfeaturecorrelations-aug-' + aug+'.npy')
-            target.write(data + ' ' + label + ' ' + corrdata + ' ' + meancorrdata + '\n')
-        else:
-            target.write(data + ' ' + label + '\n')
-
+        row={};
+        row['fileid']=  DATA.ids[subj] 
+        row['data']= data
+        if use_labels:
+            row['labels']= label
+        
+        df = df.append(row, ignore_index=True)
+        
+    output=df.merge(meta_data, on=['fileid'])
+    output=output.drop(['fileid','index'], axis=1)
+    output.to_pickle(os.path.join(indir,filename))
+        
+    
+# =============================================================================
+#         if use_correlations:
+#             corrdata=os.path.join(indir, abr + 'featurecorrelations-subj-' +DATA.ids[subj] + '-aug-' + aug + '.npy')
+#             meancorrdata=os.path.join(indir, abr + 'meanfeaturecorrelations-aug-' + aug+'.npy')
+#             #target.write(data + ' ' + label + ' ' + corrdata + ' ' + meancorrdata + '\n')
+#         else:
+#             target.write(data + ' ' + label + '\n')
+# 
+# =============================================================================

preparedata.py
@@ -67,6 +67,7 @@ def get_datalists(datalist,Ldirname,Fdirname):
             label = nibabel.load(os.path.join(Ldirname,name+paths.subjlabelname))
             labelset[:,ind] = label.darrays[0].data
 
+        # need to get rid of code for correlation naps - isn't being used!
         if paths.getFeatureCorrelations:
             correlationset =np.zeros((numdatapoints,(len(datalist)+1)))
             # estimate correlation maps showing agreement of individual subject data with group
@@ -77,7 +78,9 @@ def get_datalists(datalist,Ldirname,Fdirname):
         
             
         for d in range(0,func.numDA):
+            
             if paths.remove_outliers:
+                # this is really hacky - not recommended - need better way
                 func.darrays[d].data=cm.remove_outliers(d, func.darrays[d].data)
             # fill data array
             dataset.DATA[:,start+d] = func.darrays[d].data
@@ -100,7 +103,7 @@ def get_datalists(datalist,Ldirname,Fdirname):
     return ALLDATA
     
     
-def project_data(DATA, interpolator, Odir, abr, aug):
+def project_data(DATA, interpolator, Odir, abr, aug,meta_csv,data_csv):
     """
                project data from sphere to plane
 
@@ -108,29 +111,29 @@ def project_data(DATA, interpolator, Odir, abr, aug):
                Parameters
                ----------
                data_set         : struct containing single subject featuresets
-               label_set        : struct containing label files
-               correlation_set  : struct containing correlation maps
-                                  (showing agreement between subject data and that of group)
                interpolator     : links spherical mesh grid points to coordinates on 2D project
                                   (currently only nearest neighbour available)
-               Odir             : output directory
+               Ofile            : output file
                abr              : file naming convention
                aug              : numerical indexing of files by augmentation
 
                Returns
                -------
        """
-    n_end = '.txt'
+    
     cm.project_data(DATA, interpolator, Odir, paths.resampleH, paths.resampleW,paths.lons, abr, aug, paths.usegrouplabels,paths.normalise)
+    
     if paths.normalise:
-        n_end='normalised.txt'
+        data_csv.replace('.pk1', 'normalised.pk1')
 
 
-    if paths.usegrouplabels:
-        filename = os.path.join(Odir, 'projections'+ aug+n_end)
-    else:
-        filename = os.path.join(Odir, 'projections'+ aug+'Nature' +n_end)
-
-    cm.write_projection_paths(DATA['data'], filename, Odir, abr, aug,paths.usegrouplabels, paths.getFeatureCorrelations,paths.normalise)
+# =============================================================================
+#     if paths.usegrouplabels:
+#         filename = os.path.join(Odir, 'projections'+ aug+n_end)
+#     else:
+#         filename = os.path.join(Odir, 'projections'+ aug+'Nature' +n_end)
+# 
+# =============================================================================
+    cm.write_projection_paths(DATA['data'], data_csv, Odir, abr, aug,paths.use_labels,paths.usegrouplabels, paths.getFeatureCorrelations,paths.normalise,meta_csv)
 
 